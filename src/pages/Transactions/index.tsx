0febe69fd3c555bde03d482ffb6179d50ab44ef4
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2024-03-21 21:49:22 -0300

    Conexão com a API - Realizando requisição HTTP

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,16 @@
+
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Transactions() {
+    const { transactions } = useContext(TransactionsContext)
+    
     return (
+        
         <div>
             <Header />
             <Summary />
@@ -13,40 +19,23 @@ export function Transactions() {
                 <SearchForm />
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/04/2022</td>
-                        </tr>
-                        <tr>
-                            <td width='50%'>Hamburguer</td>
-                            <td>
-                                <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
-                            </td>
-                            <td>Alimentação</td>
-                            <td>10/04/2022</td>
-                        </tr>
+                        {transactions.map(transaction => (
+                            <tr key={transaction.id}>
+                                <td width="50%">{transaction.description}</td>
+                                <td>
+                                    <PriceHighLight variant={transaction.type}>
+                                        {transaction.type === 'outcome' && '- '}
+                                        {new Intl.NumberFormat('pt-BR', {
+                                            style: 'currency',
+                                            currency: 'BRL'
+                                        }).format(transaction.price)}
+                                    </PriceHighLight>
+                                </td>
+                                <td>{transaction.category}</td>
+                                <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}</td>
+                            </tr>
+                        ))}
 
-                        <tr>
-                            <td width='50%'>Aluguel do apartamento</td>
-                            <td>
-                                <PriceHighLight variant="outcome">- R$ 1.200,00</PriceHighLight>
-                            </td>
-                            <td>Casa</td>
-                            <td>10/04/2022</td>
-                        </tr>
-
-                        <tr>
-                            <td width='50%'>Computador</td>
-                            <td>
-                                <PriceHighLight variant="income">R$ 5.400,00</PriceHighLight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/03/2022</td>
-                        </tr>
                     </tbody>
                 </TransactionsTable>
 
