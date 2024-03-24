69b58c60ca9c0d27d1d447112ad42a73e82dcf18
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2024-03-24 00:13:56 -0300

    Conexão com a API - Criando nova transação

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -9,15 +9,23 @@ interface Transaction {
     category: string
     createdAt: string
 }
+interface createTransactionInput {
+    description: string
+    price: number
+    category: string
+    type: 'income' | 'outcome'
+}
 
 interface TransactionsContextType {
     transactions: Transaction[]
     fetchTransactions: (query?: string) => Promise<void>
+    createTransaction: (data: createTransactionInput) => Promise<void>
 }
 
 interface TransactionsProviderProps {
     children: ReactNode
 }
+
 
 export const TransactionsContext = createContext({} as TransactionsContextType);
 
@@ -35,12 +43,26 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
         setTransactions(response.data)
     }
 
+    async function createTransaction(data: createTransactionInput) {
+        const { description, price, category, type } = data
+
+        const response = await api.post('/transactions', {
+            description,
+            price,
+            category,
+            type,
+            createdAt: new Date()
+        })
+        
+        setTransactions(state => [response.data, ...state])
+    }
+
     useEffect(() => {        
         fetchTransactions()
     }, [])
 
     return (
-        <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
+        <TransactionsContext.Provider value={{ transactions, fetchTransactions, createTransaction }}>
             {children}
         </TransactionsContext.Provider>
     )
